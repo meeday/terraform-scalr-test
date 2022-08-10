0bfc193a3101f9f078b6a5d6328fb7a2037a167a
Meedaxa Ahmed <meedaxa.ahmed@gmail.com>
2022-08-10 16:21:06 +0100

    dynamodb resource provisioned

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "terraform_state" {
-  bucket = "terraform-scalr-test"
+  bucket = "terraform-env0-test"
   # Enable versioning so we can see the full revision history of our
   # state files
   versioning {
@@ -16,5 +16,15 @@ resource "aws_s3_bucket" "terraform_state" {
         sse_algorithm = "AES256"
       }
     }
+  }
+}
+
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "terraform-up-and-running-locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+  attribute {
+    name = "LockID"
+    type = "S"
   }
 }
