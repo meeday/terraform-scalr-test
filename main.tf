1d223c04b7eb4a866110413a2a7a64f3614f8982
Meedaxa Ahmed <meedaxa.ahmed@gmail.com>
2022-08-10 15:48:47 +0100

    variables changed

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 provider "aws" {
-  access_key = var.TF_VAR_access_key
-  secret_key = var.TF_VAR_secret_key
+  access_key = AWS_ACCESS_KEY_ID
+  secret_key = AWS_SECRET_ACCESS_KEY
   region     = "eu-west-2"
 }
 
