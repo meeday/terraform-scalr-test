32fbb6d48ed2c3bfa7f02017e923e0e104619413
Meedaxa Ahmed <meedaxa.ahmed@gmail.com>
2022-08-10 15:46:25 +0100

    env0 test

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 provider "aws" {
-  profile = "scalr_test"
-  region  = "eu-west-2"
+  access_key = var.TF_VAR_access_key
+  secret_key = var.TF_VAR_secret_key
+  region     = "eu-west-2"
 }
-
 
 resource "aws_s3_bucket" "terraform_state" {
   bucket = "terraform-scalr-test"
@@ -20,28 +20,3 @@ resource "aws_s3_bucket" "terraform_state" {
     }
   }
 }
-
-resource "aws_dynamodb_table" "terraform_locks" {
-  name         = "terraform-up-and-running-locks"
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "LockID"
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-}
-
-terraform {
-  backend "s3" {
-    # Replace this with your bucket name!
-    bucket  = "terraform-scalr-test"
-    key     = "global/s3/terraform.tfstate"
-    region  = "eu-west-2"
-    profile = "scalr_test"
-    # Replace this with your DynamoDB table name!
-    dynamodb_table = "terraform-up-and-running-locks"
-    encrypt        = true
-  }
-}
-
-
