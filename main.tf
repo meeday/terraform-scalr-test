45e36c63470ca0ffdc5f93ec61493d3a3ddb5e60
Meedaxa Ahmed <meedaxa.ahmed@gmail.com>
2022-08-10 16:27:14 +0100

    save state to s3 bucket

--- main.tf
+++ main.tf
@@ -28,3 +28,16 @@ resource "aws_dynamodb_table" "terraform_locks" {
     type = "S"
   }
 }
+
+
+terraform {
+  backend "s3" {
+    # Replace this with your bucket name!
+    bucket = "terraform-env0-test"
+    key    = "global/s3/terraform.tfstate"
+    region = "eu-west-2"
+    # Replace this with your DynamoDB table name!
+    dynamodb_table = "terraform-up-and-running-locks"
+    encrypt        = true
+  }
+}
