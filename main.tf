5b885f2fe864b443fec8c122220b0a66a785dcf3
Meedaxa Ahmed <meedaxa.ahmed@gmail.com>
2022-08-10 16:00:39 +0100

    bucket ref changed

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "eu-west-2"
 }
 
-resource "aws_s3_bucket" "terraform_state" {
+resource "aws_s3_bucket" "env0_bucket" {
   bucket = "terraform-env0-test"
   # Enable versioning so we can see the full revision history of our
   # state files
