c16acd634c95cc2e5e99083ef111e2cbc82084b9
Meedaxa Ahmed <meedaxa.ahmed@gmail.com>
2022-08-10 15:53:19 +0100

    remove keys

--- main.tf
+++ main.tf
@@ -1,7 +1,5 @@
 provider "aws" {
-  access_key = var.AWS_ACCESS_KEY_ID
-  secret_key = var.AWS_SECRET_ACCESS_KEY
-  region     = "eu-west-2"
+  region = "eu-west-2"
 }
 
 resource "aws_s3_bucket" "terraform_state" {
