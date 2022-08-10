e6d8dafbbab2e329a4165e3d9eb317d3023b66b4
Meedaxa Ahmed <meedaxa.ahmed@gmail.com>
2022-08-10 15:52:02 +0100

    var added

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 provider "aws" {
-  access_key = AWS_ACCESS_KEY_ID
-  secret_key = AWS_SECRET_ACCESS_KEY
+  access_key = var.AWS_ACCESS_KEY_ID
+  secret_key = var.AWS_SECRET_ACCESS_KEY
   region     = "eu-west-2"
 }
 
