f3d41b89336220b52d9f2c135029d2c62fa38d84
Meedaxa Ahmed <meedaxa.ahmed@gmail.com>
2022-08-11 08:59:59 +0100

    migrate state to backend

--- main.tf
+++ main.tf
@@ -21,14 +21,14 @@ resource "aws_dynamodb_table" "terraform_locks" {
 }
 
 
-# terraform {
-#   backend "s3" {
-#     # Replace this with your bucket name!
-#     bucket = "terraform-env0-test"
-#     key    = "global/s3/terraform.tfstate"
-#     region = "eu-west-2"
-#     # Replace this with your DynamoDB table name!
-#     dynamodb_table = "terraform-env0-locks"
-#     encrypt        = true
-#   }
-# }
+terraform {
+  backend "s3" {
+    # Replace this with your bucket name!
+    bucket = "meeday-terraform-env0-example"
+    key    = "global/s3/terraform.tfstate"
+    region = "eu-west-2"
+    # Replace this with your DynamoDB table name!
+    dynamodb_table = "terraform-env0-locks"
+    encrypt        = true
+  }
+}
